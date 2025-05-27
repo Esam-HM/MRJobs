c81c954c01edc247a2064fe2e9a3828e89e64ba0
Esam-HM <isamhalimeh@gmail.com>
2025-05-27 09:06:38 +0300

    movie rating count job added

---
+++ MovieRatingCount/src/main/java/org/example/MovieRatingCountDriver.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
+public class MovieRatingCountDriver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Configuration conf = new Configuration();
+        String[] _args = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        if(_args.length!=2){
+            System.out.println("Argument Error | Argument Should Be <input> <output>");
+            System.exit(1);
+        }
+
+        Job job = new Job(conf, "Movie_Rating_Count");
+        job.setJarByClass(MovieRatingCountDriver.class);
+        job.setMapperClass(MovieRatingCountMapper.class);
+        job.setCombinerClass(MovieRatingCountReducer.class);
+        job.setReducerClass(MovieRatingCountReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(_args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(_args[1]));
+
+        boolean status = job.waitForCompletion(true);
+        if(!status){
+            System.out.println(job.getJobName() + " Job Failed to Complete.");
+            System.exit(-1);
+        }
+
+        System.out.println(job.getJobName() + " Job Completed Successfully.");
+        System.exit(0);
+    }
+}
---
+++ MovieRatingCount/src/main/java/org/example/MovieRatingCountReducer.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MovieRatingCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int count = 0;
+
+        for(IntWritable val : values){
+            count += val.get();
+        }
+
+        context.write(key, new IntWritable(count));
+    }
+}
